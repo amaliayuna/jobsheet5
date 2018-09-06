62d9d3658e63921444c733b965b1dabd6d37cdcc
Amalia Yuna <Amalia Yuna@Amaliaaa>
2018-09-06 10:54:15 +0700

    commit keseluruhan

---
+++ src/praktikum/throws_.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package praktikum;
+
+/**
+ *
+ * @author Amalia Yuna
+ */
+public class throws_ {
+       public static void main(String[] args) {
+        try
+        {
+            f();
+        }
+        catch(Exception e)
+        {
+            System.out.println(e);
+        }
+    }
+    public static void f() throws NullPointerException, ArrayIndexOutOfBoundsException
+    {
+        //implementasi method
+        throw new NullPointerException();
+        //throw new ArrayIndexOutOfBoundsException();
+    }
+    
+}
---
+++ src/praktikum/try_.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package praktikum;
+
+/**
+ *
+ * @author Amalia Yuna
+ */
+public class try_ {
+      public static void main(String [] args){
+      try {
+        int a = 1 / 0; 
+        System.out.println("perintah selanjutnya");  
+      }
+      catch (Exception kesalahan){
+          System.err.println(kesalahan);
+      }
+   } 
+    
+}
